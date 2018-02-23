0cd826ffc60443beebf0b6adfc59cac878470593
Khalid Madih <madih_khalid@hotmail.com>
2018-02-23 09:37:19 -0700

    adding connection file

---
+++ config/connection.js
@@ -0,0 +1,24 @@
+// Set up database connection
+var mysql = require('mysql');
+var connection;
+
+var connection = mysql.createConnection({
+	host: "localhost",
+	port: 3306,
+	user: "root",
+	password: "",
+	database: "burgers_db" 
+});
+
+// Establishing the connection
+connection.connect(function(err) {
+	if (err) {
+		console.error('error conencting: ' + err.stack);
+		return;
+	}
+	console.log('connected as id ' + connection.threadId);
+	console.log('PORT ' + connection.config.port);
+});
+
+// exporting the connection to orm
+module.exports = connection;
