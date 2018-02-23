6d5aac44b6c1c798f2b8cd22d9743eac63d6a603
Khalid Madih <madih_khalid@hotmail.com>
2018-02-23 10:36:45 -0700

    adding a adding in the environment variable option for JAWSDB for heroku

--- config/connection.js
+++ config/connection.js
@@ -2,13 +2,17 @@
 var mysql = require("mysql");
 var connection;
 
-var connection = mysql.createConnection({
-  host: "localhost",
-  port: 3306,
-  user: "root",
-  password: "",
-  database: "burgers_db"
-});
+// adding in the environment variable option for JAWSDB for heroku
+if (process.env.JAWSDB_URL) {
+	connection = mysql.createConnection(process.env.JAWSDB_URL);
+} else {
+	connection = mysql.createConnection({
+		host: 'localhost',
+		user: 'root',
+		password: '',
+		database: 'burgers_db'
+	});
+};
 
 // Establishing the connection
 connection.connect(function(err) {
